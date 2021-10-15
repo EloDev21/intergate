1aae6326397a397a728493516c1e5887d0f90467
Hippolyte Ledogar <hippolyte.ledogar@gmail.com>
2021-10-15 14:49:28 +0200

    Favicon correction

--- resources/nav_head.php
+++ resources/nav_head.php
@@ -7,7 +7,7 @@ ob_start();
     <!-- edge -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <title><?= $title ?? '' ?></title>
-    <link rel="icon" href="favicon.ico">
+    <link rel="icon" href="../resources/favicon.ico">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" integrity="sha512-vKMx8UnXk60zUwyUnUPM3HbQo8QfmNx7+ltw8Pm5zLusl1XIfwcxo8DbWCqMGKaWeNxWA8yrx5v3SaVpMvR3CA==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.0/font/bootstrap-icons.css">
